b35e30911491f2aa4c336fa955c4056568245316
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2026-07-15 23:43:58 -0600

    chore(mobile): inject global error handler to debug fatal js crashes

--- mobile/index.js
+++ mobile/index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Alert } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+
+const defaultErrorHandler = global.ErrorUtils.getGlobalHandler();
+global.ErrorUtils.setGlobalHandler((error, isFatal) => {
+  Alert.alert(
+    'FATAL CRASH INFO',
+    `Error: ${error.name}: ${error.message}\n\nStack:\n${error.stack ? error.stack.substring(0, 1000) : ''}`,
+    [{ text: 'OK' }]
+  );
+  if (defaultErrorHandler) {
+    defaultErrorHandler(error, isFatal);
+  }
+});
+
 import App from './App';
 import { name as appName } from './app.json';
 
