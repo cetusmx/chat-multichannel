d6c7269c5ac0c9bb6b86bc2e3321cfc097607c5e
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2026-08-20 21:16:56 -0600

    fix(mobile): deduplicate React and Zustand in Metro config to fix startup crash

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -9,6 +9,13 @@ const path = require('path');
  */
 const config = {
   watchFolders: [path.resolve(__dirname, '../frontend')],
+  resolver: {
+    extraNodeModules: {
+      'react': path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+      'zustand': path.resolve(__dirname, 'node_modules/zustand'),
+    }
+  }
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
